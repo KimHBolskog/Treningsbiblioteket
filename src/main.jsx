e83c3beacfb39403890eaf959d3042fbe172e741
KimHBolskog <blskog.kimhauger@gmail.com>
2025-11-07 19:17:52 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
+// src/main.jsx
 import React from "https://esm.sh/react@18";
 import { createRoot } from "https://esm.sh/react-dom@18/client";
-import App from "./App.jsx";
 
+// App settes globalt i App.jsx: window.App = App
 const container = document.getElementById("root");
-createRoot(container).render(<App />);
+createRoot(container).render(React.createElement(window.App));
