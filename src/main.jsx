90d26640268fce9925a9a6cbe46b6d71680a88a9
KimHBolskog <blskog.kimhauger@gmail.com>
2025-11-07 17:43:38 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import BilingualSite from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BilingualSite />
+  </React.StrictMode>
+);
