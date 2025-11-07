b6075d7d3f79449db4dca3d452a47b273ad5d67c
KimHBolskog <blskog.kimhauger@gmail.com>
2025-11-07 19:08:07 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-// Bootstrap React from CDN (no bundler)
 import React from "https://esm.sh/react@18";
 import { createRoot } from "https://esm.sh/react-dom@18/client";
 import App from "./App.jsx";
