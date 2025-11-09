0231db031837cce36455e4b219a47e8be103032d
KimHBolskog <blskog.kimhauger@gmail.com>
2025-11-09 10:13:53 +0100

    Delete src directory

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-// src/main.jsx
-import React from "https://esm.sh/react@18";
-import { createRoot } from "https://esm.sh/react-dom@18/client";
-
-// App settes globalt i App.jsx: window.App = App
-const container = document.getElementById("root");
-createRoot(container).render(React.createElement(window.App));
