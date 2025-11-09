1c6ed28f452f8dd4d1d8691bced3bd4d11eb1a9d
KimHBolskog <blskog.kimhauger@gmail.com>
2025-11-09 10:31:23 +0100

    Delete main.jsx

--- main.jsx
+++
@@ -1,7 +0,0 @@
-// main.jsx (kjøres i nettleseren med Babel Standalone)
-import React from "https://esm.sh/react@18";
-import { createRoot } from "https://esm.sh/react-dom@18/client";
-import App from "./App.jsx";
-
-const container = document.getElementById("root");
-createRoot(container).render(<App />);
